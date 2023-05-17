68edc548cb016fbebcb5731749a7d963b14d48bd
man2706kum <kumarmanish19963@gmail.com>
2023-05-17 20:33:40 +0530

    funding predicate and sending transaction using predicate

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,75 @@ import React, { useEffect, useState } from "react";
 import "@fuel-wallet/sdk";
 import "./App.css";
 import { startRegistration, startAuthentication } from "@simplewebauthn/browser";
-import { Account, JsonFlatAbi, NativeAssetId, Predicate, Provider, Wallet, WalletLocked, WalletUnlocked, getRandomB256, hexlify } from "fuels";
+import { Account, Address, JsonFlatAbi, NativeAssetId, Predicate, Provider, Wallet, WalletLocked, WalletUnlocked, getRandomB256, hexlify } from "fuels";
 import { PredicateAbi__factory } from './types';
 
 
 function App() {
+  const [connected, setConnected] = useState<boolean>(false);
+  const [account, setAccount] = useState<string>("");
+  const [counter, setCounter] = useState<number>(0);
+  const [loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      checkConnection();
+      setLoaded(true);
+    }, 200)
+
+  }, [connected])
+
+  //to use the predicate in different async functions.
+  let pred: any;
+  let pubKey: any;
+
+  async function fundPredicate() {
+    if (window.fuel) {
+     try {
+        await window.fuel.connect();
+        const [account] = await window.fuel.accounts();
+        const wallet = await window.fuel!.getWallet(account);
+        // Fund the predicate
+        const response1 = await wallet.transfer(pred.address, 10000, NativeAssetId, {
+          gasLimit: 164,
+          gasPrice: 1,
+        });
+        await response1.wait();
+        console.log("predicateAddress from fundPredicate: " + pred.address);
+        setAccount(account);
+        setConnected(true);
+     } catch(err) {
+       console.log("error connecting: ", err);
+     }
+    }
+   }
+
+   async function send(addr: any) {
+    const someAddress = Address.fromDynamicInput(addr);
+    console.log(someAddress);
+
+    if (window.fuel) {
+     try {
+      await window.fuel.connect();
+        // Try to spend the funds in the predicate
+        console.log(pred.address);
+        const tx = await pred
+          .setData(pubKey)
+          .transfer(someAddress, 300, NativeAssetId, {
+            gasLimit: 3000,
+            gasPrice: 1,
+          });
+        await tx.waitForResult();
+
+        setAccount(account);
+        setConnected(true);
+     } catch(err) {
+       console.log("error connecting: ", err);
+     }
+    }
+   }
+
+
   async function btnRegBegin() {
     fetch("/generate-registration-options")
       .then(async (res) => {
@@ -47,7 +111,8 @@ function App() {
         const configurable = { PUBKEY: pubkeyArray };
         const { abi, bin } = PredicateAbi__factory;
         const predicate = new Predicate(bin, abi, provider, configurable);
-    
+        pred = predicate;
+        pubKey = pubkeyArray;
         // set predicate data to be the same as the configurable constant
         predicate.setData(configurable.PUBKEY);
 
@@ -89,18 +154,48 @@ function App() {
 
   }
   
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    const addr = e.target.address.value;
+    // console.log(addr);
+    send(addr);
+  }
+
+  async function checkConnection() {
+    if (window.fuel) {
+      const isConnected = await window.fuel.isConnected();
+      if (isConnected) {
+        const [account] = await window.fuel.accounts();
+        setAccount(account);
+        setConnected(true);
+      }
+    }
+  }
+
+  if (!loaded) return null
+
   return (
     <>
       <div className="App">
             <>
               <button style={buttonStyle} onClick={btnRegBegin}>
               Register
-              </button>
+              </button><br />
 
               <button style={buttonStyle} onClick={btnAuthBegin}>
               Authenticate
-              </button>
+              </button><br />
 
+              <button style={buttonStyle} onClick={fundPredicate}>
+              fundPredicate
+              </button><br /><br />
+
+              <form onSubmit={handleSubmit}><input type="text" name = "address" placeholder ="wallet address" /><br />
+              <button style={buttonStyle}><br />
+              send
+              </button>
+              </form>
             </>
       </div>
     </>

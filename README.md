0a03890b93da6d9535fcb0b6cc9fbe70ea72fbb7
Elena Fuentes Bongenaar <e.fuentesbongenaar@fastmail.com>
2023-06-16 18:26:02 -0600

    Script works with configurable constant. It takes as input the data coming from WebAuthn. Data conversion must be fixed as verification is failing. Verification within script works correctly (when ran with test data inside the script)

--- README.md
+++ README.md
@@ -2,31 +2,46 @@
 
 Simple webapp that has a connection with the Fuel blockchain (Wallet in browser and predicate loaded) & webauthn integrated.
 
+## Description of current functionality (Fuel side)
+
+There is a smart contract deployd on beta-3 testnet (id = 0xc433fa483a6b91c1e48f84c648ee00b0bf469a38ed65c1cad595842ea3b0bf68), contract code can be found [here](https://github.com/hashcloak/fuel-crypto/blob/verifying_contract/contract/src/main.sw). 
+
+Then there is a script that calls that smart contract. This is currently called from the frontend. 
+
+The next step is to create a predicate that calls the script / verifies the script was executed successfully.
+
+### Next steps
+
+#### Make sure verification in script works 
+The script is called correctly, as in the parameters have the right size and come from the correct origin. However, the signature verification fails. There might be an issue with data conversion as everything in the frontend is written differently than in Rust (script). 
+This is an example of test values, but once again they are in Rust: https://github.com/hashcloak/fuel-crypto/blob/p256/testing/p256_tests/verifying_test/tests/harness.rs#L155
+Note: with these test values directly used in the script, verification passes
+
+#### Create Predicate
+
+The predicate will represent the Burner address and should call the script / make sure the script was executed successfully. 
+
+#### Use of Fuel wallet
+
+With this setup, a Fuel wallet is required (for loading Contract and Script). Is there a way around this? Is this functionality OK?
+
 ## App Functionality
 
-This small app has the following functionality:
+- Connect Fuel Wallet; this is necessary to instantiate the Script that is used
+- Create Burner Wallet; does WebAuthn registration + instantiates script with created public key
+- Send funds; does WebAuthn authentication + call the main function on the script with the signature received from WebAuthn. Since the script needs the contract, also the contract is loaded in this step. 
 
-- user can click on Register, this does registration with WebAuthn and creates a predicate using the public key from WebAuthn
-- user can click on Authentication, this only does authentication with WebAuthn for the moment.
-
-TODO:
-- send funds using Authentication with WebAuthn, which results in a signature, which will be validated by the predicate and makes the transaction valid
-
-Expected workings:
-- click on Register: should give a popup in browser for registration. Successfully going through these steps should give a message in the console "Registration successful", print the pubkey and create the predicate based on the pubkey
-- click on Authenticate when registered: should give a popup for authentication. Successfully going through these steps should give a message in the console "Authentication successful" 
-- when Registration or Authentication fails it should give that as an error in the console
-
+For loading both script and contract a Fuel wallet is required. When the Fuel wallet extension is added to the browser, a request for connection pops up at the first time of running. 
 
 ## Codebase explanation + instructions
 
-The codebase consists of 3 parts:
-- predicate; this is where the Sway predicate lives. It is a simple example taken from [documentation](https://fuellabs.github.io/fuels-ts/guide/predicates/) for the moment
+The (WIP!) codebase consists of several parts:
+- predicate; this has to be updated. The original idea was to do verification directly in the script but that is too heavy of an operation at this moment for a predicate to take. So it will be changed to calling the contract. Folder p256 was added initially to have direct access to the necessary cryptography, but will probably not be necessary
+script; calls the smart contract deployed on beta-3 testnet, additionally does a check that the set (configurable) pubkey is the same as the one of the signature
 - server; this is where the calls to webauthn live. Absolutely not production ready, it even has some hardcoded `expectedOrigin`s. This does registration and authentication, following an example repo
 - frontend; the visual part that ties it all together. Calls both the server that does WebAuthn stuff & creates the predicate that is the burner wallet
 
-
-### 1a. Predicate code
+<!-- ### 1a. Predicate code
 
 THIS WAS SOLUTION OPTION A, WHICH DOESN'T WORK WITH CURRENT STATE OF ECOSYSTEM.
 
@@ -48,9 +63,9 @@ Make sure that in `frontend/src/types/index.ts` only 1 line similar to:
 `
 export { PredicateAbi__factory } from './factories/PredicateAbi__factory';
 `
-exists. Remove any redundant lines. 
+exists. Remove any redundant lines.  -->
 
-### 1B. Script code
+### 1. Script code
 
 Solution B: have the verification in a smart contract, and let the script call the verification function. The predicate should use/verify execution of script. 
 
@@ -70,7 +85,7 @@ npx fuels typegen -i ../**/**/script-abi.json  -o ./src/script_types --script
 
 Make sure that in `frontend/src/script_types/index.ts` only 1 line similar to:
 `
-export { ContractAbi__factory } from './factories/ContractAbi__factory';
+export { ScriptAbi__factory } from './factories/ScriptAbi__factory';
 `
 exists. Remove any redundant lines. 
 
@@ -92,7 +107,7 @@ Tutorial: https://fuelbook.fuel.network/master/quickstart/frontend.html
 
 Needs to run on fuels >= 0.42.0.
 
-Make sure that the typings for the predicate have been generated and that in `frontend/src/types/index.ts` only 1 `PredicateAbi__factory` is exported. 
+Make sure that the typings for the script have been generated and that in `frontend/src/script_types/index.ts` only 1 `ScriptAbi__factory` is exported. 
 Furthermore, the server must be running on 127.0.0.1:8000, if it is somewhere else, this has to be adjusted in `frontend/package.json`, specifically `"proxy": "http://127.0.0.1:8000"`. 
 
 Open the app in a browser where the Fuel wallet extension has been added and WebAuthn is supported. Open the console in the Developer Tools to see additional messages. 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,13 @@ import React, { useEffect, useState } from "react";
 import "@fuel-wallet/sdk";
 import "./App.css";
 import { startRegistration, startAuthentication } from "@simplewebauthn/browser";
-import { Account, Address, JsonFlatAbi, NativeAssetId, Predicate, Provider, Wallet, WalletLocked, WalletUnlocked, getRandomB256, hexlify, hashMessage, arrayify, BN, bn, BigNumberish, Script, Contract, AbstractContract, ContractFactory } from "fuels";
-import { PredicateAbi__factory } from './types';
+import { Address, NativeAssetId, Predicate, Provider, Wallet, hexlify, hashMessage, arrayify, Script, Contract } from "fuels";
 import { AsnParser } from '@peculiar/asn1-schema';
 import { ECDSASigValue } from '@peculiar/asn1-ecc';
 import base64url from 'base64url';
 // import { signature_bytesInput } from "./types/factories/PredicateAbi__factory";
 // 
-import { ContractAbi__factory } from "./contracts";
-import { AffinePointInput, ContractAbi, FieldElementInput, PublicKeyInput, ScalarInput, SignatureInput } from "./contracts/ContractAbi";
-import { VerifyPublicKeyInput, sign } from "crypto";
+import { ContractAbi, ContractAbi__factory } from "./contracts";
 import { ScriptAbi__factory } from "./script_types";
 
 // The address of the contract deployed the Fuel testnet
@@ -80,7 +77,6 @@ function App() {
     }
    }
 
-
    async function send(addr: any, amount: number) {
     const inputAddress = Address.fromDynamicInput(addr);
     const walletInputAddress = Wallet.fromAddress(inputAddress, provider);
@@ -128,14 +124,6 @@ function App() {
 
     // parse the string as an object
     const clientDataObj = JSON.parse(decodedClientData);
-
-    // console.log(clientDataObj);
-    // console.log(clientDataObj.challenge);
-    // console.log(hashMessage(clientDataObj.challenge));
-    // console.log(arrayify(hashMessage(clientDataObj.challenge)));
-    // console.log(new TextEncoder().encode(clientDataObj.challenge));
-    // console.log(new Uint8Array(parsedSignature.r));
-    // console.log(new Uint8Array(parsedSignature.s).byteLength);
 
     //parsing the signature 
     let s = new Uint8Array(parsedSignature.s);
@@ -191,75 +179,21 @@ function App() {
       await window.fuel.connect();
       // Invoke script
         if (script) {
+          // FYI 
+          // u64 = 16602909452612575158
+          // equals bn([230, 105, 98, 46, 96, 242, 159, 182])
+          
+          const wallet = await window.fuel.getWallet(account);
+          const contract = ContractAbi__factory.connect(CONTRACT_ID, wallet);
+      
+          setContract(contract);
+          console.log('Contract connected')
 
-          let hash1 = '0xaf2bdbe1aa9b6ec1e2ade1d694f41fc71a831d0268e9891562113d8a62add1bf';
-          // [
-          //   175, 43, 219, 225, 170, 155, 110, 193, 226, 173, 225, 214, 148, 244, 31, 199, 26, 131,
-          //   29, 2, 104, 233, 137, 21, 98, 17, 61, 138, 98, 173, 209, 191,
-          // ];
-          const x: FieldElementInput = {
-            ls: [
-                bn([182, 159, 242, 96, 46, 98, 105, 230]), // 16602909452612575158
-                bn([230, 105, 98, 46, 96, 242, 159, 182]), // 16602909452612575158 THIS WORKS
-                // bn([108, 250, 97, 59, 146, 184, 73, 192]), //13855808666783054444
-                bn([104, 109, 53, 198, 116, 235, 97, 201]),//14511138361138572648,
-                bn([49, 157, 90, 37, 186, 212, 254, 96])//6989257567681289521,
-            ],
-          };
-          const y: FieldElementInput = {
-            ls: [
-              bn([153, 34, 70, 212, 148, 194, 163, 119]), //8620948056189575833
-              bn([81, 159, 126, 45, 12, 178, 241, 242]), //17505968991938453329
-              bn([100, 188, 40, 86, 233, 233, 26, 164]), //11825020959996820580
-              bn([153, 188, 184, 8, 16, 254, 3, 121]) //8720092648338668697
-            ],
-          };
-          const a: AffinePointInput = {
-            x: x, y: y, infinity: 0
-          };
-          const p: PublicKeyInput = {
-            point: a
-          };
-          const vk = {
-            inner: p,
-          };
-  
-          let r1 : ScalarInput = {
-            ls: [
-              bn([22, 55, 175, 78, 168, 14, 77, 195]),//14072920526640068374
-              bn([145, 249, 170, 86, 123, 135, 44, 157]),//11325576126734727569
-              bn([214, 129, 94, 212, 156, 221, 64, 17]),//1243237162801856982
-              bn([253, 168, 182, 172, 42, 139, 212, 239])//17281590685529975037
-            ],
-          };
-          let s1 : ScalarInput = {
-              ls: [
-                bn([168, 205, 58, 132, 47, 171, 196, 77]),//5603792056925998504
-                bn([6, 244, 175, 185, 219, 0, 233, 243]),//17575579964503225350
-                bn([101, 159, 226, 182, 161, 199, 54, 212]),//15291629082155065189
-                bn([65, 124, 101, 45, 148, 28, 203, 247])//17855396570382826561
-              ],
-          };
-          let sign1 : SignatureInput = { r: r1, s: s1 };
-          //old params p, sign1, msg
-          const res = await script!.functions.main(p, sign1, msg)
+          const res = await script!.functions.main(pubKey, r_string, s_string, msg_string)
               .txParams({ gasPrice: 1, gasLimit: 500000000})
               .addContracts([contract! as Contract])
               .call();
-          // console.log(value); //42,0,0 WORKS!!!!
-          // So what works now is calling a script that calls the contract CONTINUE HERE
           console.log(res);
-
-          // const tx = await predicate!.
-          //   setData(pubKey).
-          //   transfer(inputAddress, amount, NativeAssetId, {gasPrice: 1});
-          // const result = await tx.waitForResult();
-
-          // if (result.status.type =='success') {
-          //   console.log('success');
-          // } else {
-          //   console.log('fail');
-          // }
 
           // TODO WHEN IT WORKS Check balance of someAddress was indeed increased
           const destinationBalance = await walletInputAddress.getBalance(NativeAssetId);
@@ -311,34 +245,23 @@ function App() {
         }
         console.log(pubkeyArray);
 
-        // USING PREDICATE
-        // Create Burner Wallet
-        // const provider = new Provider('https://beta-3.fuel.network/graphql');
-        // setProvider(provider);
-        // const configurable = { PUBKEY: pubkeyArray };
-        // const { abi, bin } = PredicateAbi__factory;
-        // const pred = new Predicate(bin, abi, provider, configurable);
-
-        // setBurnerWalletAddress(pred.address.toString());
-
-        // setPredicate(pred);
-        // setPubKey(pubkeyArray);
         if (window.fuel) {
 
           const wallet = await window.fuel.getWallet(account);
 
-          // CREATE SCRIPT
+          // CREATE SCRIPT + SET PUBKEY
           const provider = new Provider('https://beta-3.fuel.network/graphql');
           setProvider(provider);
           const configurable = { PUBKEY: pubkeyArray };
           const { abi, bin } = ScriptAbi__factory;
           const script = new Script(bin, abi, wallet);
-          // script.setConfigurableConstants(configurable);
+          script.setConfigurableConstants(configurable);
           
           console.log(script.functions);
 
-          // THIS IS WRONG, IT JUST TAKES THE SAME WALLET ADDRESS
-          setBurnerWalletAddress(wallet.address.toString());
+          // TODO create predicate and let that be the burner address
+          // The predicate calls the script or verifies that the script was executed
+          // setBurnerWalletAddress();
 
           setScript(script);
           setPubKey(pubkeyArray);
@@ -367,112 +290,6 @@ function App() {
     }
   }
 
-  async function connectContract() {
-    if (window.fuel) {
-      const wallet = await window.fuel.getWallet(account);
-      const contract = ContractAbi__factory.connect(CONTRACT_ID, wallet);
-      
-      setContract(contract);
-      console.log('Contract connected')
-
-      let hash1 = '0xaf2bdbe1aa9b6ec1e2ade1d694f41fc71a831d0268e9891562113d8a62add1bf';
-      // [
-      //   175, 43, 219, 225, 170, 155, 110, 193, 226, 173, 225, 214, 148, 244, 31, 199, 26, 131,
-      //   29, 2, 104, 233, 137, 21, 98, 17, 61, 138, 98, 173, 209, 191,
-      // ];
-
-      // TODO How to get the right values here?
-      const fe_x: FieldElementInput = {
-        ls: [
-            bn([182, 159, 242, 96, 46, 98, 105, 230]), // 16602909452612575158
-            bn([108, 250, 97, 59, 146, 184, 73, 192]), //13855808666783054444
-            bn([104, 109, 53, 198, 116, 235, 97, 201]),//14511138361138572648,
-            bn([49, 157, 90, 37, 186, 212, 254, 96])//6989257567681289521,
-        ],
-      };
-      const fe_y: FieldElementInput = {
-        ls: [
-          bn([153, 34, 70, 212, 148, 194, 163, 119]), //8620948056189575833
-          bn([81, 159, 126, 45, 12, 178, 241, 242]), //17505968991938453329
-          bn([100, 188, 40, 86, 233, 233, 26, 164]), //11825020959996820580
-          bn([153, 188, 184, 8, 16, 254, 3, 121]) //8720092648338668697
-        ],
-      };
-      const a: AffinePointInput = {
-        x: fe_x, y: fe_y, infinity: 0
-      };
-      const pubkey: PublicKeyInput = {
-        point: a
-      };
-      const vk = {
-        inner: pubkey,
-      };
-
-      let r1 : ScalarInput = {
-        ls: [
-          bn([22, 55, 175, 78, 168, 14, 77, 195]),//14072920526640068374
-          bn([145, 249, 170, 86, 123, 135, 44, 157]),//11325576126734727569
-          bn([214, 129, 94, 212, 156, 221, 64, 17]),//1243237162801856982
-          bn([253, 168, 182, 172, 42, 139, 212, 239])//17281590685529975037
-        ],
-      };
-      let s1 : ScalarInput = {
-          ls: [
-            bn([168, 205, 58, 132, 47, 171, 196, 77]),//5603792056925998504
-            bn([6, 244, 175, 185, 219, 0, 233, 243]),//17575579964503225350
-            bn([101, 159, 226, 182, 161, 199, 54, 212]),//15291629082155065189
-            bn([65, 124, 101, 45, 148, 28, 203, 247])//17855396570382826561
-          ],
-      };
-      let sign1 : SignatureInput = { r: r1, s: s1 };
-      
-      try {
-        let res = await contract.functions.verify_signature(pubkey, sign1, hash1).txParams({ gasPrice: 1, gasLimit: 500000000}).call();
-        console.log('signature verified called');
-        console.log(res);
-      } catch(err) {
-        console.log("error sending transaction...", err);
-        /*error sending transaction... Error: Expected returnReceipt
-
-        {
-          "doc": "https://docs.rs/fuel-asm/latest/fuel_asm/enum.PanicReason.html#variant.OutOfGas",
-          "reason": "OutOfGas"
-        }
-
-        Receipts:
-        [
-          {
-            "type": "Call",
-            "from": "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "to": "0xc433fa483a6b91c1e48f84c648ee00b0bf469a38ed65c1cad595842ea3b0bf68",
-            "amount": "0x0",
-            "assetId": "0x0000000000000000000000000000000000000000000000000000000000000000",
-            "gas": "0xbebbf8a",
-            "param1": "0x9ee5b619",
-            "param2": "0x3868",
-            "pc": "0x43f8",
-            "is": "0x43f8"
-          },
-          {
-            "type": "Panic",
-            "id": "0xc433fa483a6b91c1e48f84c648ee00b0bf469a38ed65c1cad595842ea3b0bf68",
-            "reason": "0x22d4d2450000000",
-            "pc": "0xe63f4",
-            "is": "0x43f8",
-            "contractId": "0x0000000000000000000000000000000000000000000000000000000000000000"
-          },
-          {
-            "type": "ScriptResult",
-            "result": "0x2",
-            "gasUsed": "0xbebc0c5"
-          }
-        ]
-        */
-      }
-    } else {
-      console.log('No wallet connected')
-    }
-  }
 
   if (!loaded) return null
 
@@ -481,8 +298,6 @@ function App() {
       <div className="App">
             <>
               <button style={buttonStyle} onClick={connect}>Connect Fuel Wallet</button>
-
-              <button style={buttonStyle} onClick={connectContract}>Connect Contract</button>
 
               <button style={buttonStyle} onClick={btnRegBegin}>
               Create Burner Wallet
@@ -495,9 +310,9 @@ function App() {
                   </div>
               }
 
-              <button style={buttonStyle} onClick={fundPredicate}>
+              {/* <button style={buttonStyle} onClick={fundPredicate}>
               Fund burner wallet (in order to test)
-              </button><br /><br />
+              </button><br /><br /> */}
 
               <form onSubmit={handleSubmit}>
                 <input type="text" name = "address" placeholder ="destination address" /><br /><br />
